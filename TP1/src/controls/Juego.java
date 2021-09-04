65b133c13f36d271051ef7334d788a9ddbfee3c9
frderickk <federicofarias@outlook.com>
2021-09-04 02:35:56 -0300

    Comentario en clase Juego para descarga de fuente

--- TP1/src/controls/Juego.java
+++ TP1/src/controls/Juego.java
@@ -11,7 +11,7 @@ public class Juego {
 	public static void main(String[] args) {
 		inicio = new VentanaPrincipal();
 		inicio.comienzoDelJuego.setText("Comenzar  el juego!");
-		// no creo que tengas la fuente ArcadeClassic, cualquier cosa dejo link de descarga
+		// no creo que tengas la fuente ArcadeClassic, cualquier cosa dejo link de descarga abajo
 		// https://www.dafont.com/arcade-classic-2.font
 		inicio.comienzoDelJuego.setFont(new Font("ArcadeClassic", Font.PLAIN, 15));
 		inicio.setVisible(true);
